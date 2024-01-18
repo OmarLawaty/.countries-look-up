87445d280ec62e52f547cf87a0a8060061cdf256
Omar Lawatey <omarlawatey@gmail.com>
2024-01-18 04:36:34 +0200

    Fix: Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Flex } from '@chakra-ui/react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { CountryPreview, Home } from './pages';
 import { Header } from './components';
